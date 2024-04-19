d57b0725ca8d6a3c1a0ee10d4355a8888bb5a67e
Albert  Brugnara <albert@brugnara.at>
2024-04-19 16:01:47 +0200

    Update texts.js
    Test delete gampopa

--- assets/app/texts.js
+++ assets/app/texts.js
@@ -1,5 +1,5 @@
 const texts = [
-  { id: "1_gompopa", url: "content/1_gompopa.xml" },
+  
   { id: "2_dignaga", url: "content/2_dignaga.xml" },
   { id: "3", url: "content/3_ganden.xml" },
   { id: "4", url: "content/4_shrisena.xml" },
